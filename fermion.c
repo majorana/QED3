65a161447fcaa75d8c14f179f0d6048db13c0693
Meng Cheng <cmsir1986@gmail.com>
2015-07-18 13:42:02 -0700

    Rewrite fermion det and force

--- fermion.c
+++ fermion.c
@@ -14,9 +14,8 @@ void fermion(complex double *out, complex double *in)
 {
 	int i,j;
   	for(i=0; i<GRIDPOINTS; i++) {
-		out[i] = exp(-g_mu)*Ut[i]*in[tp[i]] - in[i] 
-			- g_t*(Ux[i]*in[xp[i]] + cconj(Ux[xm[i]])*in[xm[i]] + Uy[i]*in[yp[i]] + cconj(Uy[ym[i]])*in[ym[i]]);
-		//printf("%d,%f,%f\n", i, creal(out[i]),cimag(out[i]));
+		out[i] = in[i] -exp(-g_mu*dt)*Ut[i]*in[tp[i]] 
+			+ dt*Ut[i]*(Ux[tp[i]]*in[tp[xp[i]]] + cconj(Ux[tp[xm[i]]])*in[tp[xm[i]]] + Uy[tp[i]]*in[tp[yp[i]]] + cconj(Uy[tp[ym[i]]])*in[tp[ym[i]]]);
 	}
 	return;
 }
@@ -31,8 +30,8 @@ void fermion_herm(complex double *out, complex double *in)
 {
 	int i;
   	for(i=0; i<GRIDPOINTS; i++) {
-		out[i] = exp(-g_mu)*cconj(Ut[tm[i]])*in[tm[i]] - in[i]
-			- g_t*(cconj(Ux[xm[i]])*in[xm[i]] + cconj(Uy[ym[i]])*in[ym[i]] + Ux[i]*in[xp[i]] + Uy[i]*in[yp[i]]);
+		out[i] =  in[i] - exp(-g_mu*dt)*cconj(Ut[tm[i]])*in[tm[i]]
+			+ dt*(cconj(Ut[tm[xm[i]]]*Ux[xm[i]])*in[tm[xm[i]]] + cconj(Ut[tm[ym[i]]]*Uy[ym[i]])*in[tm[ym[i]]] + cconj(Ut[tm[xp[i]]])*Ux[i]*in[tm[xp[i]]] + cconj(Ut[tm[yp[i]]])*Uy[i]*in[tm[yp[i]]]);
 	}
 	return;
 }
@@ -47,23 +46,24 @@ void fermion_sqr(complex double *out, complex double *temp, complex double *in)
 void fermion_DGx(complex double *out, complex double *in, int j)
 {
 	set_zero(out);
-	out[j] = -I*g_t*Ux[j]*in[xp[j]];
-	out[xp[j]] = I*g_t*cconj(Ux[j])*in[j];
+	out[tm[j]] = I*Ux[j]*in[xp[j]];
+	out[tm[xp[j]]] = -I*cconj(Ux[j])*in[j];
 	return;
 }
+
 
 void fermion_DGy(complex double *out, complex double *in, int j)
 {
 	set_zero(out);
-	out[j] = -I*g_t*Uy[j]*in[yp[j]];
-	out[yp[j]] = I*g_t*cconj(Uy[j])*in[j];
+	out[tm[j]] = I*Uy[j]*in[yp[j]];
+	out[tm[yp[j]]] = -I*cconj(Uy[j])*in[j];
 	return;
 }
 
 void fermion_DGt(complex double *out, complex double *in, int j)
 {
 	set_zero(out);
-	out[j] = I*exp(-g_mu)*Ut[j]*in[tp[j]];
+	out[j] = -I*exp(-g_mu*dt)*Ut[j]*(in[tp[j]] + dt*(Ux[tp[j]]*in[tp[xp[j]]] + Uy[tp[j]]*in[tp[yp[j]]] + cconj(Ux[tp[xm[j]]])*in[tp[xm[j]]] + cconj(Uy[tp[ym[j]]])*in[tp[ym[j]]] ) );
 	return;
 }
 
